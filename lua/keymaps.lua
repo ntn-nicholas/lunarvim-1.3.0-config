7be5330dc05d8c13e8add167b4b9e4d50bb63b2e
Nicholas Nguyen <nguyennicholas8@gmail.com>
2023-08-24 23:30:16 -0700

    Debugger stepping

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -14,11 +14,18 @@ lvim.keys.insert_mode["<A-h>"] = "<Left>"
 lvim.keys.insert_mode["<A-l>"] = "<Right>"
 
 -- Runs a file
-lvim.keys.normal_mode["<F8>p"] = ':w<CR>:!python3.9 %<CR>'
+lvim.keys.normal_mode["<F8>"] = function()
+  local filetype = vim.bo.filetype
+  if filetype == 'c' or filetype == 'cpp' then
+    vim.cmd('make %< CFLAGS="-g -Wall"')
+  else
+    print(filetype, "cannot be compiled.")
+  end
+end
 lvim.keys.normal_mode["<F10>"] = function()
   local filetype = vim.bo.filetype
   if filetype == 'c' or filetype == 'cpp' then
-    vim.cmd('make %<')
+    vim.cmd('make %< CFLAGS="-g -Wall" && ./%<')
   elseif filetype == 'rust' then
     vim.cmd('write')
     vim.cmd('!cargo run')
@@ -31,9 +38,10 @@ lvim.keys.normal_mode["<F10>"] = function()
   end
 end
 
+
 -- Debugger shortcuts
-lvim.keys.normal_mode["<F5>"] = "<cmd>DapStepInto"
-lvim.keys.normal_mode["<F6>"] = "<cmd>DapStepOver"
+lvim.keys.normal_mode["<F5>"] = "<CMD>DapStepInto<CR>"
+lvim.keys.normal_mode["<F6>"] = "<CMD>DapStepOver<CR>"
 
 -- Change tab
 lvim.keys.normal_mode["<M-l>"] = ':bn<CR>'
