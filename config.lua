c1ab834db3e5f95706c5fcd62e9409fc4eb5d962
Nicholas Nguyen <nguyennicholas8@gmail.com>
2023-10-19 23:32:25 +0000

    Add jupyter-notebook-like functionalities

--- config.lua
+++ config.lua
@@ -71,15 +71,15 @@ local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
     -- for input mode
     i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
+        ["<Tab>"] = actions.move_selection_next,
+        ["<S-Tab>"] = actions.move_selection_previous,
         ["<C-n>"] = actions.cycle_history_next,
         ["<C-p>"] = actions.cycle_history_prev,
     },
     -- for normal mode
     n = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
+        ["<Tab>"] = actions.move_selection_next,
+        ["<S-Tab>"] = actions.move_selection_previous,
     },
 }
 
--- lua/my_plugins.lua
+++ lua/my_plugins.lua
@@ -336,6 +336,27 @@ lvim.plugins = {
   "nvim-neotest/neotest",
   "nvim-neotest/neotest-python",
 
+  {
+    "GCBallesteros/NotebookNavigator.nvim",
+    keys = {
+      { "]h", function() require("notebook-navigator").move_cell "d" end },
+      { "[h", function() require("notebook-navigator").move_cell "u" end },
+      { "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
+      { "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
+    },
+    dependencies = {
+      "echasnovski/mini.comment",
+      "hkupty/iron.nvim", -- repl provider
+      -- or "akinsho/toggleterm.nvim" -- repl provider
+      "anuvyklack/hydra.nvim",
+    },
+    event = "VeryLazy",
+    config = function()
+      local nn = require "notebook-navigator"
+      nn.setup({ activate_hydra_keys = "<leader>h", repl_provide = "iron" })
+    end,
+  },
+
   -- %% JAVA %%
 
   -- Java JDTLS
